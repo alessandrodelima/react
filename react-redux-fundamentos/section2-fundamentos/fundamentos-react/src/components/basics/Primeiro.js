0fdb0b03a716e1b0075c7beee7ca5a4089356d60
Alessandro Lima <alessandro.lima@msn.com>
2020-12-08 12:56:49 -0300

    Component com JSX

--- react-redux-fundamentos/section2-fundamentos/fundamentos-react/src/components/basics/Primeiro.js
+++ react-redux-fundamentos/section2-fundamentos/fundamentos-react/src/components/basics/Primeiro.js
@@ -1,3 +1,10 @@
+import React from 'react';
 export default function Primeiro() {
-  return 'Primeiro Component mais novo';
+  const msg = 'Seja bem vindo(a)!'
+  return (
+     <div>
+        <h2>Primeiro component com JSX</h2>
+        <p>{ msg }</p>
+    </div>
+  )
 }
